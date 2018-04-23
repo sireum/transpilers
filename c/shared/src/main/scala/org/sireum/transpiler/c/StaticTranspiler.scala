a94fb4d55308deacb16c7dad8993734916acad26
Robby <robby@k-state.edu>
2018-04-23 15:37:49 -0500

    Checkpointing StaticTranspiler.

--- c/shared/src/main/scala/org/sireum/transpiler/c/StaticTranspiler.scala
+++ c/shared/src/main/scala/org/sireum/transpiler/c/StaticTranspiler.scala
@@ -26,6 +26,7 @@ object StaticTranspiler {
   val unitType: ST = st"void"
   val bType: ST = st"B"
   val zType: ST = st"Z"
+  val empty: ST = st""
 }
 
 import StaticTranspiler._
@@ -67,36 +68,163 @@ import StaticTranspiler._
     }
     cclassMap = cclassMap + owner ~> cclass(
       header = cclass.header(headers = cclass.header.headers + id ~> (headers :+ st"$header;")),
-      impl = cclass.impl(impls = cclass.impl.impls + id ~> (impls :+ impl)))
+      impl = cclass.impl(impls = cclass.impl.impls + id ~> (impls :+ impl))
+    )
+  }
+
+  @pure def transpileExp(exp: AST.Exp, _nextTempNum: Z): (ST, Z, ISZ[ST]) = {
+    var stmts = ISZ[ST]()
+    var nextTempNum = _nextTempNum
+
+    def freshTemp(isClone: B, tpe: AST.Typed, expOpt: Option[ST]): ST = {
+      val p = freshTempName(nextTempNum)
+      nextTempNum = p._2
+      val rhs: ST = expOpt match {
+        case Some(e) => if (isClone) st" = *($e)" else st" = $e"
+        case _ => st""
+      }
+      stmts = stmts :+ st"${transpileType(tpe, isClone)} ${p._1}$rhs;"
+      return p._1
+    }
+
+    def transLitB(exp: AST.Exp.LitB): ST = {
+      return st"?" // TODO
+    }
+
+    def transLitZ(exp: AST.Exp.LitZ): ST = {
+      return st"?" // TODO
+    }
+
+    def transBinary(exp: AST.Exp.Binary): ST = {
+      return st"?" // TODO
+    }
+
+    def transUnary(exp: AST.Exp.Unary): ST = {
+      return st"?" // TODO
+    }
+
+    def transInvoke(exp: AST.Exp.Invoke): ST = {
+      return st"?" // TODO
+    }
+
+    val expST: ST = exp match {
+      case exp: AST.Exp.LitB => val r = transLitB(exp); r
+      case exp: AST.Exp.LitZ => val r = transLitZ(exp); r
+      case exp: AST.Exp.Binary => val r = transBinary(exp); r
+      case exp: AST.Exp.Unary => val r = transUnary(exp); r
+      case exp: AST.Exp.Invoke => val r = transInvoke(exp); r
+      case _ => halt("TODO") // TODO
+    }
+
+    return (expST, nextTempNum, stmts)
   }
 
   @pure def transpileStmt(stmt: AST.Stmt): ISZ[ST] = {
-    return ISZ() // TODO
+    var stmts = ISZ[ST]()
+    var nextTempNum = z"0"
+
+    def transExp(exp: AST.Exp): ST = {
+      val t = transpileExp(exp, nextTempNum)
+      nextTempNum = t._2
+      stmts = stmts ++ t._3
+      return t._1
+    }
+
+    def freshTemp(isClone: B, tpe: AST.Typed, expOpt: Option[ST]): ST = {
+      val p = freshTempName(nextTempNum)
+      nextTempNum = p._2
+      val rhs: ST = expOpt match {
+        case Some(e) => if (isClone) st" = *($e)" else st" = $e"
+        case _ => st""
+      }
+      stmts = stmts :+ st"${transpileType(tpe, isClone)} ${p._1}$rhs;"
+      return p._1
+    }
+
+    def transVar(stmt: AST.Stmt.Var, init: AST.Stmt.Expr): Unit = {
+      val t: AST.Typed = stmt.tipeOpt match {
+        case Some(tipe) => tipe.typedOpt.get
+        case _ => init.typedOpt.get
+      }
+      val isClone = isMutable(t)
+      val rhs = transExp(init.exp)
+      stmts = stmts :+ st"${transpileType(t, isClone)} ${localName(stmt.id.value)} = ${if (isClone) st"*($rhs)" else rhs};"
+    }
+
+    def transVarComplex(stmt: AST.Stmt.Var): Unit = {
+      halt("TODO") // TODO
+    }
+
+    stmts = stmts :+ empty
+    stmt.posOpt match {
+      case Some(pos) => stmts = stmts :+ st"// L${pos.beginLine}"
+      case _ => stmts = stmts :+ st"// L?"
+    }
+    stmt match {
+      case stmt: AST.Stmt.Var =>
+        stmt.initOpt.get match {
+          case init: AST.Stmt.Expr => transVar(stmt, init)
+          case _ => transVarComplex(stmt)
+        }
+      case _ => halt("TODO") // TODO
+    }
+
+    return stmts
+  }
+
+  @pure def isMutable(t: AST.Typed): B = {
+    t match {
+      case t: AST.Typed.Name =>
+        if (t.ids == AST.Typed.msName) {
+          return T
+        } else {
+          th.typeMap.get(t.ids) match {
+            case Some(info: TypeInfo.Sig) => return !info.ast.isImmutable
+            case Some(info: TypeInfo.AbstractDatatype) => return !info.ast.isDatatype
+            case _ => return F
+          }
+        }
+      case t: AST.Typed.Tuple =>
+        var i = 0
+        val sz = t.args.size
+        while (i < sz) {
+          if (isMutable(t.args(i))) {
+            return T
+          }
+          i = i + 1
+        }
+        return F
+      case _ => return F
+    }
   }
 
   @pure def methodHeader(method: AST.ResolvedInfo.Method): ST = {
     val name = methodName(method)
     val tpe = method.tpeOpt.get
-    val retType = transpileType(tpe.ret)
+    val retType = transpileType(tpe.ret, T)
     val params: ST =
       if (method.paramNames.isEmpty)
         unitType
       else
         st"${(
           for (p <- ops.ISZOps(tpe.args).zip(method.paramNames))
-            yield st"${transpileType(p._1)} ${localName(p._2)}",
+            yield st"${transpileType(p._1, F)} ${localName(p._2)}",
           ", "
         )}"
     return st"$retType $name($params)"
   }
 
-  @pure def transpileType(tpe: AST.Typed): ST = {
+  @pure def transpileType(tpe: AST.Typed, isLValue: B): ST = {
     tpe match {
       case AST.Typed.unit => return unitType
       case AST.Typed.b => return bType
       case AST.Typed.z => return zType
       case _ => halt("TODO") // TODO
     }
+  }
+
+  @pure def freshTempName(nextTempNum: Z): (ST, Z) = {
+    return (st"t_$nextTempNum", nextTempNum + 1)
   }
 
   @pure def methodResolvedInfo(method: AST.Stmt.Method): AST.ResolvedInfo.Method = {
