cd5736cafdd20361f3aeb65ee0c605bfef530f6f
Robby <robby@k-state.edu>
2018-05-02 21:09:01 -0500

    Updated.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-rm -fR runtime slang alir mill-standalone versions.properties
+rm -fR runtime slang alir versions.properties
 if [[ -z $SIREUM_HOME ]]; then
   echo "Please specify SIREUM_HOME env var to point to Sireum Kekinian."
   exit 1
