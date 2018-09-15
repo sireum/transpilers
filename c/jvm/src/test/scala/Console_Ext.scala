4d478177a8aeb5b3d3e17d4dfc0ff7ab89dfb7f8
Robby <robby@ksu.edu>
2018-09-15 10:43:56 -0500

    Fixed Console_Ext.

--- c/jvm/src/test/scala/Console_Ext.scala
+++ c/jvm/src/test/scala/Console_Ext.scala
@@ -2,10 +2,11 @@ import org.sireum.{ISZ, U8}
 
 object Console_Ext {
   def read(): ISZ[U8] = {
-    val in = new java.util.Scanner(System.in)
     val buffer = scala.collection.mutable.ArrayBuffer[U8]()
-    while (in.hasNextByte) {
-      buffer += U8(in.nextByte())
+    var b = System.in.read()
+    while (b != -1) {
+      buffer += U8(b)
+      b = System.in.read()
     }
     return ISZ(buffer: _*)
   }
