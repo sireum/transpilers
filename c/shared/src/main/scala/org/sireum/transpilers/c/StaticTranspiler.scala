37c8379ab478c760e46cb78fd46a9b1beebff474
Robby <robby@k-state.edu>
2019-08-28 10:28:30 -0600

    Avoid copy in match expression translation.

--- c/shared/src/main/scala/org/sireum/transpilers/c/StaticTranspiler.scala
+++ c/shared/src/main/scala/org/sireum/transpilers/c/StaticTranspiler.scala
@@ -2583,18 +2583,10 @@ import StaticTranspiler._
     }
     val t = expType(stmt.exp)
     val tpe = transpileType(t)
-    val kind = typeKind(t)
-    val tmp: ST = if (isScalar(kind)) {
-      stmts = stmts :+ st"$tpe $temp = $e;"
-      temp
-    } else {
-      stmts = stmts :+ st"DeclNew$tpe($temp);"
-      stmts = stmts :+ st"Type_assign(&$temp, $e, sizeof(${typeDecl(t)}));"
-      st"&$temp"
-    }
+    stmts = stmts :+ st"$tpe $temp = $e;"
     stmts = stmts :+ st"B $handled = F;"
     for (cas <- stmt.cases) {
-      transCase(handled, tmp, cas)
+      transCase(handled, temp, cas)
     }
     stmts = stmts :+ st"""sfAssert($handled, "Error when pattern matching.");"""
   }
